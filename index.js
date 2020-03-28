16b6f75c4464ea9982f442330366bd7a4b46c569
Psalmspire <psalmuel4praiz@gmail.com>
2020-03-28 10:09:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var name = "Psalmspire";
+
+var courses = ["PHP","Javascript","HTML","C#","CSS","Design","Kotlin"]
+
+const data = [];
+
+var number = courses.length;
+
+for (var i = 1; i < 200; i +=2)
+{
+data.push(i)
+}
+
+console.log("My name is " + name);
+
+console.log("I offer " + courses);
+
+console.log("I offer " + number, " courses in all");
+console.log(data);
+
+
+
+
+
+
